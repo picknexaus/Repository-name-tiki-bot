ffe51afbd3660e02886bcfffcf55f29fdf4c52c5
picknexaus <picknexaus@gmail.com>
2026-04-30 23:44:52 +0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,35 @@
+const cron = require("node-cron");
+const products = require("./products");
+const postToWP = require("./wp");
+
+// random sản phẩm
+function getRandom() {
+  return products[Math.floor(Math.random() * products.length)];
+}
+
+// tạo bài viết
+function buildPost(p) {
+  return `
+    <h2>${p.title}</h2>
+    <p>🔥 Sản phẩm hot hôm nay trên Tiki</p>
+    <a href="${p.link}" target="_blank">
+      👉 MUA NGAY
+    </a>
+  `;
+}
+
+// chạy bot
+async function runBot() {
+  const p = getRandom();
+
+  await postToWP(p.title, buildPost(p));
+}
+
+// chạy test ngay
+runBot();
+
+// chạy mỗi ngày 9h sáng
+cron.schedule("0 9 * * *", () => {
+  runBot();
+  console.log("⏰ Bot chạy auto mỗi ngày");
+});
