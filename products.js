3f963fd1e64e3b17344f62cb7826355f3496cbae
picknexaus <picknexaus@gmail.com>
2026-05-01 00:50:40 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,10 +1,6 @@
 module.exports = [
   {
-    title: "🔥 Deal hot Tiki hôm nay",
-    link: "https://ti.ki/qpJevlMg/2LWIV8Q1"
-  },
-  {
-    title: "⚡ Ưu đãi thương hiệu giảm sốc",
-    link: "https://ti.ki/6lqKKEKZ/LL6C6C4U"
+    title: "🔥 Deal hot Shopee hôm nay",
+    link: "https://s.shopee.vn/6VKLAmA3sn"
   }
 ];
