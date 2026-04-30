345c00f2bebc0279c90237202e841c143d332b83
picknexaus <picknexaus@gmail.com>
2026-04-30 23:54:15 +0700

    Update products.js

--- products.js
+++ products.js
@@ -6,9 +6,5 @@ module.exports = [
   {
     title: "⚡ Ưu đãi thương hiệu giảm sốc",
     link: "https://ti.ki/6lqKKEKZ/LL6C6C4U"
-  },
-  {
-    title: "🎯 Sản phẩm hot bán chạy",
-    link: "https://ti.ki/qpJevlMg/2LWIV8Q1"
   }
 ];
