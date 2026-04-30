e0b891e298f131b1508c3bd1438f48587ed5fce0
picknexaus <picknexaus@gmail.com>
2026-04-30 23:43:49 +0700

    Create products.js

---
+++ products.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    title: "🔥 Deal hot Tiki hôm nay",
+    link: "https://ti.ki/qpJevlMg/2LWIV8Q1"
+  },
+  {
+    title: "⚡ Ưu đãi thương hiệu giảm sốc",
+    link: "https://ti.ki/6lqKKEKZ/LL6C6C4U"
+  },
+  {
+    title: "🎯 Sản phẩm hot bán chạy",
+    link: "https://ti.ki/qpJevlMg/2LWIV8Q1"
+  }
+];
